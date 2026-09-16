b7696a6a3ad55006a6d732995ea918904af6902f
azadamirazad99-ur <azadamirazad99@gmail.com>
2026-09-16 16:59:20 +0500

    Update EnglishPosya.lua

--- EnglishPosya.lua
+++ EnglishPosya.lua
@@ -1752,7 +1752,11 @@ local government = {
 local criminal = {
     {name = '💀 Tambov Base #1', x = 2425, y = -1922, z = 22},
     {name = '💀 Kurgan Base', x = 2285, y = 1369, z = 11},
-    {name = '🖤 Black Market', x = -2249, y = 245, z = 24.5},
+    {name = '💀 Caucasian', X:-2336.90
+Y:30.49
+Z:21.15} 
+
+{name = '🖤 Black Market', x = -2249, y = 245, z = 24.5},
     {name = '⚔️ Bizzwar Zone Sawmill', x = -1043.95, y = -2464.63, z = 28.48}
 }
 
