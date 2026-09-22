37fe1ce158dc240aaea6f1ce8d3d71b772c0c21d
azadamirazad99-ur <azadamirazad99@gmail.com>
2026-09-22 11:38:13 +0500

    Update EnglishPosya.lua

--- EnglishPosya.lua
+++ EnglishPosya.lua
@@ -1718,6 +1718,7 @@ local publicPlaces = {
 local hiddenteleports = {
   {name = '🔒 ADMIN ZONE', x = -701.82, y = -2438.95, z = 1198.15},
   {name = '🔓 JAIL ESCAPE POINT', x = 644.01, y = 1049.21, z = 1208.26},
+{name = '🔒 APD CELL', x = 29.06, y = 1127.80, z = 1200.38},
   {name = '🚔 HIGH SECURITY PRISON', x = -1830, y = -2870, z = 45},
   {name = '⚔️ MILITARY SECTOR #1', x = -1092.7, y = -2539.2, z = 28.6},
   {name = '⚔️ MILITARY SECTOR #2', x = -1002.8, y = -2497.4, z = 28.1},
